59030a4e7b480f1b453bff4637824d9bf2397251
danielufeli <danielufeli@yahoo.com>
2022-01-19 23:00:48 +0100

    setup jest add test

---
+++ src/test/addTodo.test.js
@@ -0,0 +1,47 @@
+import addTodo from '../modules/addTodo.js';
+
+jest.mock('../modules/localstorage');
+
+const data = [
+  {
+    description: 'Book 1',
+    index: 1,
+    completed: false,
+  },
+  {
+    description: 'Book 2',
+    index: 2,
+    completed: true,
+  },
+  {
+    description: 'Book 3',
+    index: 3,
+    completed: false,
+  },
+];
+
+describe('Add...', () => {
+//   test('Abc to empty items', () => {
+//     const input = { value: 'Abc' };
+//     addTodo(input);
+//     expect(itemsEmpty.length).toBe(1);
+//   });
+
+  test('Abc to full items', () => {
+    const input = { value: 'Abc' };
+    const itemsFull = [
+      {
+        description: 'xyzzy',
+        complete: false,
+        index: 0,
+      },
+      {
+        description: 'abcde',
+        complete: true,
+        index: 1,
+      },
+    ];
+    addTodo(input);
+    expect(itemsFull.length).toBe(3);
+  });
+});
