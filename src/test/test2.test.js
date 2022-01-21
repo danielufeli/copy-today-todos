13c4557a351c31a73dfb7d813691404575bb8f9a
lisandroseia <lisandroseia11@gmail.com>
2022-01-21 15:19:05 -0300

    Add test case for clear all completeds

--- src/test/test2.test.js
+++ src/test/test2.test.js
@@ -6,6 +6,8 @@ import addTodo from '../modules/addTodoTest.js';
 import getTodos from '../modules/getTodos.js';
 import updateTask from '../modules/updateTask.js';
 import statusUpdate from '../modules/updateTodos.js';
+import clearCompletedTask from '../modules/clearCompletedTask.js';
+import { add } from 'lodash';
 
 jest.mock('../__mocks__/localstorage');
 jest.mock('../__mocks__/list');
@@ -48,5 +50,34 @@ describe('Complete Status Check', () => {
     statusUpdate('1');
     const completed = JSON.parse(localStorage.getItem('todos'))[0].completed;
     expect(completed).toBe(false);
+  });
+});
+
+describe('clear all completed', () => {
+  test('calling clear() should remove all completed', () => {
+    document.body.innerHTML = '<ul class="todoList"></ul>';
+    getTodos();
+    const checked = document.getElementById('1');
+    checked.click();
+    statusUpdate('1');
+    clearCompletedTask();
+    const todos = JSON.parse(localStorage.getItem('todos')).length;
+    expect(todos).toBe(0);
+  });
+
+  test('dom should contain 1 item', () => {
+    document.body.innerHTML = '<ul class="todoList"></ul>';
+    document.body.innerHTML = '<ul class="todoList"></ul>';
+    const input = { value: 'lisandro' };
+    const input2 = {value: 'daniel'};
+    addTodo(input);
+    addTodo(input2)
+    getTodos();
+    const checked = document.getElementById('1');
+    checked.click();
+    statusUpdate('1');
+    clearCompletedTask();
+    const dom = document.querySelectorAll('li');
+    expect(dom.length).toBe(1)
   });
 });
