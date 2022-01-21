0d8fd4ee8f8117c2e4d5680ecf91b9b886e69668
danielufeli <danielufeli@yahoo.com>
2022-01-21 19:06:13 +0100

    Add Complete test for false and true

--- src/test/test2.test.js
+++ src/test/test2.test.js
@@ -2,25 +2,51 @@
  * @jest-environment jsdom
  */
 
- import addTodo from '../modules/addTodoTest.js';
- import getTodos from '../modules/getTodos.js';
- import updateTask from '../modules/updateTask.js';
- 
- import removeTodos from '../modules/removeTodos.js';
- 
- jest.mock('../__mocks__/localstorage');
- jest.mock('../__mocks__/list');
+import addTodo from '../modules/addTodoTest.js';
+import getTodos from '../modules/getTodos.js';
+import updateTask from '../modules/updateTask.js';
+import statusUpdate from '../modules/updateTodos.js';
 
- describe('editing', () => {
-     test('edit 1 should save a new desc',() => {
-        document.body.innerHTML = '<ul class="todoList"></ul>';
-        const input = { value: 'lisandro' };
-        addTodo(input);
-         getTodos();
-        updateTask('daniel','1');
-        getTodos();
-        const item = JSON.parse(localStorage.getItem('todos'))[0].description;
-        expect(item).toMatch("daniel");
-         
-     }) 
- })
+jest.mock('../__mocks__/localstorage');
+jest.mock('../__mocks__/list');
+
+describe('editing', () => {
+  test('edit 1 should save a new desc', () => {
+    document.body.innerHTML = '<ul class="todoList"></ul>';
+    const input = { value: 'lisandro' };
+    addTodo(input);
+    updateTask('daniel', '1');
+    getTodos();
+    const item = JSON.parse(localStorage.getItem('todos'))[0].description;
+    expect(item).toMatch('daniel');
+  });
+
+  test('Test Updated DOM', () => {
+    document.body.innerHTML = '<ul class="todoList"></ul>';
+    getTodos();
+    const descriptionInput = document.body.querySelector('.task-des');
+    expect(descriptionInput.value).toBe('daniel');
+  });
+});
+
+describe('Complete Status Check', () => {
+  test('Set Completed to true', () => {
+    document.body.innerHTML = '<ul class="todoList"></ul>';
+    getTodos();
+    const checked = document.getElementById('1');
+    checked.click();
+    statusUpdate('1');
+    const completed = JSON.parse(localStorage.getItem('todos'))[0].completed;
+    expect(completed).toBe(true);
+  });
+
+  test('Set Completed to false', () => {
+    document.body.innerHTML = '<ul class="todoList"></ul>';
+    getTodos();
+    const checked = document.getElementById('1');
+    checked.click();
+    statusUpdate('1');
+    const completed = JSON.parse(localStorage.getItem('todos'))[0].completed;
+    expect(completed).toBe(false);
+  });
+});
